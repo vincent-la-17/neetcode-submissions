5dc83beea89996e426ec598252778c4542693e60
vincent-la-17 <lavincent117@gmail.com>
2026-09-24 16:25:53 -0400

    Add: min-cost-climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-6.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        for i in range(len(cost)-3, -1, -1):
+            #at each step, add the min cost of the next one or two steps
+            cost[i] += min(cost[i+1], cost[i+2])
+        return min(cost[0], cost[1])
